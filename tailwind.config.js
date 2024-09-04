8df269148263f65fa278a479840bd41e72fdee4d
Abdullah Al Mamun Akand <97316146+mamun-akand@users.noreply.github.com>
2024-09-05 02:31:02 +0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
